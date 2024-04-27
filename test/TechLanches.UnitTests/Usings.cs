17e8c004ae1eb1aa6410ffe58a27b6ee30dddf32
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2024-04-26 21:01:32 -0300

    add projeto de testes

---
+++ test/TechLanches.UnitTests/Usings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using TechLanches.Application.DTOs;
+global using TechLanches.Application.Ports.Repositories;
+global using TechLanches.Domain.Aggregates;
+global using TechLanches.Domain.Services;
+global using TechLanches.Domain.Validations;
+global using TechLanches.Domain.ValueObjects;
+global using TechLanches.Core;
