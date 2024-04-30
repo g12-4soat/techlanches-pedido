63b9b3a1f35f2b1f138a2c52e5d55f378fc0ab99
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2024-04-30 16:37:09 -0300

    remoção pastas

--- test/TechLanches.UnitTests/Usings.cs
+++
@@ -1,7 +0,0 @@
-global using Xunit;
-global using TechLanches.Core;
-global using TechLanches.Domain.Aggregates;
-global using TechLanches.Domain.Constantes;
-global using TechLanches.Domain.Enums;
-global using TechLanches.Domain.ValueObjects;
-global using TechLanches.UnitTests.Fixtures;
