a41ba786b329870161ab85ba707c5e02bf3fc9f6
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2024-06-25 20:13:56 -0300

    modificações de cliente não identificado / cliente inativo

--- src/TechLanches.Pedido/Core/TechLanches.Application/Presenters/PedidoPresenter.cs
+++ src/TechLanches.Pedido/Core/TechLanches.Application/Presenters/PedidoPresenter.cs
@@ -1,9 +1,9 @@
 ﻿using Mapster;
 using TechLanches.Application.DTOs;
-using TechLanches.Application.Gateways;
 using TechLanches.Application.Gateways.Interfaces;
 using TechLanches.Application.Presenters.Interfaces;
 using TechLanches.Domain.Aggregates;
+using TechLanches.Domain.Constantes;
 
 namespace TechLanches.Application.Presenters
 {
@@ -28,20 +28,32 @@ namespace TechLanches.Application.Presenters
             return lista;
         }
 
+        public PedidoResponseDTO ParaDto(Pedido pedido)
+        {
+            var dto = pedido.Adapt<PedidoResponseDTO>();
+            dto.ClienteCpf = RetornarCpfCliente(pedido);
+            return dto;
+        }
+
         private static async Task<PedidoResponseDTO> PreencherPedido(Pedido entidade, IPagamentoGateway pagamentoGateway)
         {
-            //framework de mapeamento não consegue lidar com value objects
             var dto = entidade.Adapt<PedidoResponseDTO>();
-            dto.ClienteCpf = entidade.Cpf.Numero;
+            dto.ClienteCpf = RetornarCpfCliente(entidade);
             dto.Pagamento = await pagamentoGateway.RetornarPagamentoPorPedidoId(entidade.Id);
             return dto;
         }
 
-        public PedidoResponseDTO ParaDto(Pedido pedido)
+        private static string RetornarCpfCliente(Pedido pedido)
         {
-            var dto = pedido.Adapt<PedidoResponseDTO>();
-            dto.ClienteCpf = pedido.Cpf.Numero;
-            return dto;
+            return ClienteNaoIdentificavel(pedido)
+               ? Constants.USER_NAO_IDENTIFICADO            
+               : pedido.Cpf.Numero; //framework de mapeamento não consegue lidar com value objects
+
+            static bool ClienteNaoIdentificavel(Pedido pedido)
+            {
+                return pedido.Cpf.Numero == Constants.USER_DEFAULT
+                    || pedido.ClienteInativo;
+            }
         }
     }
 }
